ddcbc5d0db76d25a03ccbcd612351e4d4142b7ba
Sem Rekkers <sem@devments.com>
2015-09-07 15:37:25 +0200

    src: add vector implementation

---
+++ src/vector.c
@@ -0,0 +1,48 @@
+#include "vector.h"
+
+#define AT(v,i) (v->data + (v->isize * (i)))
+
+static inline void expand(Vec *v, uint req) {
+    uint need = v->length + req;
+    if (need > v->cap) {
+        uint nsize = v->cap;
+        do {
+            nsize <<= 1;
+        } while (need > nsize);
+        void *nptr = realloc(v->data, nsize * v->isize);
+        CHECK(nptr);
+        v->data = nptr;
+        v->cap = nsize;
+    }
+}
+
+void vec_init(Vec *v, uint itemSize, uint cap) {
+    CHECK(v);
+    uint t = itemSize * len;
+    v->data = malloc(t);
+    CHECK(v->data);
+    v->isize = itemSize;
+    v->length = 0;
+    v->cap = cap;
+}
+
+void vec_destroy(Vec *v) {
+    CHECK(v);
+    free(v->data);
+}
+
+void vec_add(Vec *v, uint count, void *data) {
+    CHECK(v);
+    CHECK(data);
+    expand(v, count);
+    uint t = v->isize * count;
+    memcpy(AT(v, v->length), data, t);
+    v->length += count;
+}
+
+void* vec_at(Vec *v, uint i) {
+    CHECK(v);
+    if (i >= v->length)
+        return NULL;
+    return AT(v, i);
+}
--- src/vector.h
+++ src/vector.h
@@ -7,6 +7,13 @@ typedef struct {
     uchar *data;    // pointer to data
     uint isize;     // size of one item in bytes
     uint length;    // amount of items
+    uint cap;       // vector capacity
 } Vec;
+
+void vec_init(Vec *v, uint itemSize, uint len);
+void vec_destroy(Vec *v);
+
+void vec_add(Vec *v, uint count, void *data);
+void* vec_at(Vec *v, uint i);
 
 #endif // FIDDLE_VECTOR_H_
